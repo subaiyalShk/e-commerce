48301da6aec4b54073b5a2a416784def97a7ba81
Subaiyal Sheikh <subaiyalshk@gmail.com>
2024-06-04 16:22:09 -0500

    converted typescript and added graphql endpoint

---
+++ pages/api/hello.ts
@@ -0,0 +1,4 @@
+
+export default function (req, res, next) {
+    res.status(200).json({name: "John Doe"})
+}
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+
+const Home: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+    )
+};
+
+export default Home;
